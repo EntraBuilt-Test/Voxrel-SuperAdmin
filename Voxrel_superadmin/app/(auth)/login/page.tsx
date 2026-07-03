5aeef17a51c9325378feb0017548df4fd40fb3a6
EntraBuilt-Test <entrabuilt@gmail.com>
2026-07-03 18:14:01 +0530

    Update page.tsx

--- Voxrel_superadmin/app/(auth)/login/page.tsx
+++ Voxrel_superadmin/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
 import { LoginForm } from "@/components/blocks/login.block"
+import { AuroraLoginShell } from "@/components/aurora/AuroraLoginShell"
 
 export default function LoginPage() {
     return (
-        <LoginForm />
+        <AuroraLoginShell>
+            <LoginForm />
+        </AuroraLoginShell>
     )
 }
